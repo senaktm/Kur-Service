f4388d89e5ce4d460bada64f9ac8b112cf64ac0f
Sena Oktem <72103654+senaktm@users.noreply.github.com>
2024-08-14 19:26:03 +0300

    Kur MVC

---
+++ KurMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KurMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+			using (KurServiceReference.KurServiceClient serviceClient = new KurServiceReference.KurServiceClient())
+			{
+				if (TempData["Kurlar"] != null)
+				{
+					ViewBag.Kurlar = (List<double>)TempData["Kurlar"];
+				}
+				else
+				{
+					ViewBag.Kurlar = new List<double>();
+				}
+				var model = serviceClient.ParaBirimleriniGetir().ToList();
+				return View(model);
+			}
+		}
+		public ActionResult KurGetir(string paraBirimi)
+		{
+			using (KurServiceReference.KurServiceClient serviceClient = new KurServiceReference.KurServiceClient())
+			{
+				var model = serviceClient.KurlariGetir(paraBirimi).ToList();
+				TempData["Kurlar"] = model;
+				return RedirectToAction("Index", "Home");
+			}
+		} 
+    }
+}
---
+++ KurMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KurMVC.MvcApplication" Language="C#" %>
